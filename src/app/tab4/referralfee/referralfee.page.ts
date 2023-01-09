9df9018dd4ce2e9303551df3bad9afa787f961f6
unknown <raka.ardiansyah23@gmail.com>
2023-01-09 17:19:31 +0700

    edit tambahan penyesuaian

--- src/app/tab4/referralfee/referralfee.page.ts
+++ src/app/tab4/referralfee/referralfee.page.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
 
 @Component({
   selector: 'app-referralfee',
   templateUrl: './referralfee.page.html',
   styleUrls: ['./referralfee.page.scss'],
 })
+
 export class ReferralfeePage implements OnInit {
 
-  constructor() { }
+  constructor(private popCtrl: PopoverController) { }
 
   ngOnInit() {
   }
+
+  async _openPopover(ev: any) {
+    // console.log ("popover works!")
+    const popover = await this.popCtrl.create({
+      component: PopoverController,
+      event:ev
+    })
+
+    popover.present()
+    return await popover.present()
+  }
+
 
 }
